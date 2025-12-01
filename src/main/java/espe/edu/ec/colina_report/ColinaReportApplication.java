6ab5a664b14ddbb38662c4a8659702b2e1a48844
Mateo Ruben Colina Morales <mrcolina@espe.edu.ec>
2025-12-01 17:25:37 -0500

    Primer commit con el proyecto en blanco

---
+++ src/main/java/espe/edu/ec/colina_report/ColinaReportApplication.java
@@ -0,0 +1,13 @@
+package espe.edu.ec.colina_report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColinaReportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColinaReportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/espe/edu/ec/colina_report/ColinaReportApplicationTests.java
@@ -0,0 +1,13 @@
+package espe.edu.ec.colina_report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColinaReportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
